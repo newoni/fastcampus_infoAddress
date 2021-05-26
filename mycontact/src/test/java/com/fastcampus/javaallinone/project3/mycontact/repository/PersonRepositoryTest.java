9707119a82e44397c8d1a7b7bf69fed1102e0d04
newoni <newoni_@naver.com>
2021-05-26 22:07:01 +0900

    person service mock test 1

--- mycontact/src/test/java/com/fastcampus/javaallinone/project3/mycontact/repository/PersonRepositoryTest.java
+++ mycontact/src/test/java/com/fastcampus/javaallinone/project3/mycontact/repository/PersonRepositoryTest.java
@@ -23,28 +23,46 @@ class PersonRepositoryTest {
     private PersonRepository personRepository;
 
     @Test
-    void curd(){
-        Person person = new Person();
-        person.setName("john");
+    void findByName(){
+        List<Person> people = personRepository.findByName("tony");
+        assertThat(people.size()).isEqualTo(1);
 
-        personRepository.save(person);
-        List<Person> result = personRepository.findByName("john");
+        Person person = people.get(0);
+        assertAll(
+                ()-> assertThat(person.getName()).isEqualTo("tony"),
+                ()-> assertThat(person.getHobby()).isEqualTo("reading"),
+                ()-> assertThat(person.getAddress()).isEqualTo("seoul"),
+                ()-> assertThat(person.getBirthday()).isEqualTo(Birthday.of(LocalDate.of(1991,7,10))),
+                ()-> assertThat(person.getJob()).isEqualTo("officer"),
+                ()-> assertThat(person.getPhoneNumber()).isEqualTo("010-2222-5555"),
+                ()-> assertThat(person.isDeleted()).isEqualTo(false)
 
-        assertThat(result.size()).isEqualTo(1);
-        assertThat(result.get(0).getName()).isEqualTo("john");
-//        assertThat(result.get(0).getAge()).isEqualTo(10);
-
+        );
     }
-
 
     @Test
-    void findByBirthdatBetween(){
-        List<Person> result = personRepository.findByMonthOfBirthday(8);
+    void findByNameIfDeleted(){
+        List<Person> people = personRepository.findByName("andrew");
 
-        assertThat(result.size()).isEqualTo(2);
-        assertThat(result.get(0).getName()).isEqualTo("martin");
-        assertThat(result.get(1).getName()).isEqualTo("sophia");
-
+        assertThat(people.size()).isEqualTo(0);
     }
 
+    @Test
+    void findByMonthOfBirthDay(){
+        List<Person> people = personRepository.findByMonthOfBirthday(7);
+
+        assertThat(people.size()).isEqualTo(2);
+        assertAll(
+                ()-> assertThat(people.get(0).getName()).isEqualTo("david"),
+                ()-> assertThat(people.get(1).getName()).isEqualTo("tony")
+        );
+    }
+
+    @Test
+    void findPeopleDeleted(){
+        List<Person> people = personRepository.findPeopleDeleted();
+
+        assertThat(people.size()).isEqualTo(1);
+        assertThat(people.get(0).getName()).isEqualTo("andrew");
+    }
 }
--- mycontact/src/test/java/com/fastcampus/javaallinone/project3/mycontact/service/PersonServiceTest.java
+++ mycontact/src/test/java/com/fastcampus/javaallinone/project3/mycontact/service/PersonServiceTest.java
@@ -1,7 +1,14 @@
 package com.fastcampus.javaallinone.project3.mycontact.service;
 
 import com.fastcampus.javaallinone.project3.mycontact.domain.Person;
+import com.fastcampus.javaallinone.project3.mycontact.repository.PersonRepository;
+import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -9,23 +16,23 @@ import java.util.List;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
-@SpringBootTest
+@ExtendWith(MockitoExtension.class)
 class PersonServiceTest {
-    @Autowired
+    @InjectMocks
     private PersonService personService;
 
+    @Mock
+    private PersonRepository personRepository;
     @Test
     void getPeopleByNames(){
+        Mockito.when(personRepository.findByName("martin"))
+                .thenReturn(Lists.newArrayList(new Person("martin")));
+
         List<Person> result = personService.getPeopleByName("martin");
 
         assertThat(result.size()).isEqualTo(1);
         assertThat(result.get(0).getName()).isEqualTo("martin");
     }
 
-    @Test
-    void getPerson(){
-        Person person = personService.getPerson(3L);
 
-        assertThat(person.getName()).isEqualTo("dennis");
-    }
 }
