bbe20d3e4e06e6628877882109278dfdc1acbff7
newoni <newoni_@naver.com>
2021-05-28 23:53:23 +0900

    Mokito Test 2 with verify method for void return method <21.05.28>

--- mycontact/src/test/java/com/fastcampus/javaallinone/project3/mycontact/service/PersonServiceTest.java
+++ mycontact/src/test/java/com/fastcampus/javaallinone/project3/mycontact/service/PersonServiceTest.java
@@ -1,10 +1,12 @@
 package com.fastcampus.javaallinone.project3.mycontact.service;
 
+import com.fastcampus.javaallinone.project3.mycontact.controller.dto.PersonDto;
 import com.fastcampus.javaallinone.project3.mycontact.domain.Person;
 import com.fastcampus.javaallinone.project3.mycontact.repository.PersonRepository;
 import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
@@ -12,7 +14,9 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
@@ -34,5 +38,32 @@ class PersonServiceTest {
         assertThat(result.get(0).getName()).isEqualTo("martin");
     }
 
+    @Test
+    void getPerson(){
+        Mockito.when(personRepository.findById(1L))
+                .thenReturn(Optional.of(new Person("martin")));
+
+        Person person = personService.getPerson(1L);
+        assertThat(person.getName()).isEqualTo("martin");
+    }
+
+    @Test
+    void getPersonIfNotFound(){
+        Mockito.when(personRepository.findById(1L))
+                .thenReturn(Optional.empty());
+
+        Person person = personService.getPerson(1L);
+
+        assertThat(person).isNull();
+    }
+
+    @Test
+    void put(){
+        PersonDto dto = PersonDto.of("martin", "programming", "판교", LocalDate.now(), "programmer", "010-1111-2222");
+        personService.put(dto);
+
+        //void 인 method에 대해서 test 하는 Mokito의 특이한 방법
+        Mockito.verify(personRepository, Mockito.times(1)).save(ArgumentMatchers.any(Person.class));
+    }
 
 }
