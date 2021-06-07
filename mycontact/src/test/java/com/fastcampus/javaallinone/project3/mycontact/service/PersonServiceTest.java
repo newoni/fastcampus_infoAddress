9d208fe133be2a813e6686879f496d1dfde98919
newoni <newoni_@naver.com>
2021-06-07 23:14:35 +0900

    add custom Exception <21.06.07>

--- mycontact/src/test/java/com/fastcampus/javaallinone/project3/mycontact/service/PersonServiceTest.java
+++ mycontact/src/test/java/com/fastcampus/javaallinone/project3/mycontact/service/PersonServiceTest.java
@@ -3,6 +3,8 @@ package com.fastcampus.javaallinone.project3.mycontact.service;
 import com.fastcampus.javaallinone.project3.mycontact.controller.dto.PersonDto;
 import com.fastcampus.javaallinone.project3.mycontact.domain.Person;
 import com.fastcampus.javaallinone.project3.mycontact.domain.dto.Birthday;
+import com.fastcampus.javaallinone.project3.mycontact.exception.PersonNotFoundException;
+import com.fastcampus.javaallinone.project3.mycontact.exception.RenameNotPermittedException;
 import com.fastcampus.javaallinone.project3.mycontact.repository.PersonRepository;
 import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.Test;
@@ -69,7 +71,7 @@ class PersonServiceTest {
         Mockito.when(personRepository.findById(1L))
                 .thenReturn(Optional.empty());
 
-        assertThrows(RuntimeException.class,()-> personService.modify(1L, mockPersonDto()));
+        assertThrows(PersonNotFoundException.class,()-> personService.modify(1L, mockPersonDto()));
 
     }
 
@@ -78,7 +80,7 @@ class PersonServiceTest {
         Mockito.when(personRepository.findById(1L))
                 .thenReturn(Optional.of(new Person("tony")));
 
-        assertThrows(RuntimeException.class, ()->personService.modify(1L, mockPersonDto()));
+        assertThrows(RenameNotPermittedException.class, ()->personService.modify(1L, mockPersonDto()));
     }
 
     @Test
@@ -97,7 +99,7 @@ class PersonServiceTest {
         Mockito.when(personRepository.findById(1L))
                 .thenReturn(Optional.empty());
 
-        assertThrows(RuntimeException.class, ()-> personService.modify(1L,"daniel"));
+        assertThrows(PersonNotFoundException.class, ()-> personService.modify(1L,"daniel"));
     }
 
     @Test
@@ -115,7 +117,7 @@ class PersonServiceTest {
         Mockito.when(personRepository.findById(1L))
                 .thenReturn(Optional.empty());
 
-        assertThrows(RuntimeException.class, ()-> personService.delete(1L));
+        assertThrows(PersonNotFoundException.class, ()-> personService.delete(1L));
     }
 
     @Test
